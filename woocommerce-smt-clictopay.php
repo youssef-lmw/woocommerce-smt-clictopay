c0c3648b271f5d77ca4397936b8303611eb07cc3
youssef-lmw <49945022+youssef-lmw@users.noreply.github.com>
2020-10-14 14:57:16 +0100

    monetique-tunisie-api-v2

--- woocommerce-smt-clictopay.php
+++ woocommerce-smt-clictopay.php
@@ -11,221 +11,244 @@ Author URI: https://www.linkedin.com/in/gharbi-youssef-b38b451a2/
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-/**
- *
- * Provides a Custom Payment Gateway, mainly for testing purposes.
- */
+add_action('plugins_loaded', 'woocommerce_yg_smt_init', 0);
 
-    add_action('plugins_loaded', 'woocommerce_yg_smt_init', 0);
+function woocommerce_yg_smt_init() {
 
-    function woocommerce_yg_smt_init() {
-        if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
-        /**
-         * Gateway class
-         */
-    class WC_Yg_SMT extends WC_Payment_Gateway{
-    	protected $msg = array();
-    	public function __construct(){
+    if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
 
-    		$this->id= 'lmw_yg_smt_clictopay';
-    		$this->method_title = 'SMT ClicToPay';
-            $this->method_description = 'Accepter les paiements par ClicToPay SMT';
-            $this->icon = 'https://www.drupal.org/files/project-images/ClicToPay_logo.png';
-    		$this->has_fields = false;
-    		$this->init_form_fields();
-    		$this->init_settings();
-    		$this->title = $this->get_option('title');
-    		$this->description = $this->get_option('description');
-    		$this->affilie = $this->get_option('affilie');
-    		$this->liveurl = $this->get_option('sandbox') ? 'https://www.smt-sps.com.tn/clicktopay/' : 'https://www.smt-sps.com.tn/clicktopay/';
-    		$this->msg['message'] = "";
-    		$this->msg['class'] = "";
+    $plugin_dir = basename(dirname(__FILE__));
+    load_plugin_textdomain( 'yg-smt-woocommerce-payment-gatway', false, $plugin_dir. '/languages/' );
 
-    		add_action('init', array(&$this, 'check_smt_response'));
+    if(empty(get_woocommerce_currency_symbol('TND'))){
+        add_filter( 'woocommerce_currencies', 'add_tnd_currency' );
 
-            //update for woocommerce >2.0
-    		add_action( 'woocommerce_api_' . strtolower( get_class( $this ) ), array( $this, 'check_smt_response' ) );
-    		add_action('valid-smt-request', array(&$this, 'successful_request'));
-    		if ( version_compare( WOOCOMMERCE_VERSION, '2.0.0', '>=' ) ) {
-    			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( &$this, 'process_admin_options' ) );
-    		} else {
-    			add_action( 'woocommerce_update_options_payment_gateways', array( &$this, 'process_admin_options' ) );
-    		}
-    		add_action('woocommerce_receipt_smt', array(&$this, 'receipt_page'));
-    		add_action('woocommerce_thankyou_smt',array(&$this, 'thankyou_page'));
-    	}
+        add_filter('woocommerce_currency_symbol', 'add_tnd_currency_symbol', 10, 2);
+    }
+    /**
+     * Add the Gateway to WooCommerce
+     **/
+    function woocommerce_add_yg_smt_gateway($methods) {
+        $methods[] = 'WC_Yg_SMT';
+        return $methods;
+    }
 
-    	function init_form_fields(){
-    		$this->form_fields = array(
-    			'enabled' => array(
-    				'title' => __('Enable/Disable', 'yg'),
-    				'type' => 'checkbox',
-    				'label' => __('Enable SMT Payment Module.', 'yg'),
-    				'default' => 'no'),
-    			'title' => array(
-    				'title' => __('Title:', 'yg'),
-    				'type'=> 'text',
-    				'description' => __('This controls the title which the user sees during checkout.', 'yg'),
-    				'default' => __('SMT ClicToPay', 'yg')),
-    			'description' => array(
-    				'title' => __('Description:', 'yg'),
-    				'type' => 'textarea',
-    				'description' => __('This controls the description which the user sees during checkout.', 'yg'),
-    				'default' => __('Payment method description', 'yg')),
-    			'affilie' => array(
-    				'title' => __('Affilie', 'yg'),
-    				'type' => 'text',
-    				'description' => __('')),
-    			'sandbox' => array(
-    				'title' => __('Sandbox', 'yg'),
-    				'type' => 'checkbox',
-    				'label' => __('Enable Sandbox Mode.', 'yg'),
-    				'default' => 'no')
-    			);
-		}
+    function add_tnd_currency( $currencies ) {
+        $currencies['TND'] = __( 'Tunisian Dinar', 'woocommerce' );
+        return $currencies;
+    }
 
-        /**
-         * Admin Panel Options
-         * - Options for bits like 'title' and availability on a country-by-country basis
-         **/
-        public function admin_options(){
-     		$ipn_url = add_query_arg( 'wc-api', get_class( $this ), get_site_url() );
-
-        	echo '<h3>'.__('Clictopay SMT', 'yg').'</h3>';
-      		echo '<p>'.__( 'IPN URL: "<b><i>'.$ipn_url.'</i></b>".' ).'</p>';
-            echo '<p>Entrez les informations de configuration pour Clictopay SMT.</p>';
-
-
-        	echo '<table class="form-table">';
-        	$this->generate_settings_html();
-        	echo '</table>';
+    function add_tnd_currency_symbol( $currency_symbol, $currency ) {
+        switch( $currency ) {
+            case 'TND': $currency_symbol = 'TND'; break;
         }
+        return $currency_symbol;
+    }
 
-        /**
-         *  There are no payment fields for CCAvenue, but we want to show the description if set.
-         **/
-        function payment_fields(){
-        	if($this->description) echo wpautop(wptexturize($this->description));
+    add_filter('woocommerce_payment_gateways', 'woocommerce_add_yg_smt_gateway' );
+
+    /**
+     * Gateway class
+     */
+    class WC_Yg_SMT extends WC_Payment_Gateway {
+        public function __construct(){
+
+            $this->id = 'yg_smt';
+            $this->method_title = __('SMT ClicToPay', 'yg-smt-woocommerce-payment-gatway');
+            $this->method_description = 'Accepter les paiements par ClicToPay SMT';
+            $this->icon = 'https://lightmyweb.fr/uploads/ClicToPay_logo.png';
+            $this ->has_fields = false;
+            $this->init_form_fields();
+            $this->init_settings();
+
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->affilie = $this->get_option('affilie');
+
+            $this->liveurl = ($this->get_option('sandbox') == 'yes') ? $this->get_option('sandbox_url') : $this->get_option('production_url');
+
+            if ( version_compare( WOOCOMMERCE_VERSION, '2.0.0', '>=' ) ) {
+                add_action( 'woocommerce_api_' . strtolower( get_class( $this ) ), array( $this, 'check_smt_response' ) );
+            } else {
+                add_action('init', array(&$this, 'check_smt_response'));
+            }
+
+            if ( version_compare( WOOCOMMERCE_VERSION, '2.0.0', '>=' ) ) {
+                add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( &$this, 'process_admin_options' ) );
+            } else {
+                add_action( 'woocommerce_update_options_payment_gateways', array( &$this, 'process_admin_options' ) );
+            }
+            add_action('woocommerce_receipt_yg_smt', array(&$this, 'receipt_page'));
+        }
+        function init_form_fields(){
+
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title' => __('Enable/Disable:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type' => 'checkbox',
+                    'label' => __('Enable SMT Payment Module.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => 'no'),
+                'title' => array(
+                    'title' => __('Title:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type'=> 'text',
+                    'description' => __('This controls the title which the user sees during checkout.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => __('SMT', 'yg-smt-woocommerce-payment-gatway')),
+                'description' => array(
+                    'title' => __('Description:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type' => 'textarea',
+                    'description' => __('This controls the description which the user sees during checkout.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => __('', 'yg-smt-woocommerce-payment-gatway')),
+                'affilie' => array(
+                    'title' => __('Affiliate:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type' => 'text',
+                    'description' => __('')),
+                'production_url' => array(
+                    'title' => __('Production Url:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type'=> 'text',
+                    'description' => __('This controls the production gateway url.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => 'https://www.smt-sps.com.tn/paiement/'),
+                'sandbox_url' => array(
+                    'title' => __('Sandbox Url:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type'=> 'text',
+                    'description' => __('This controls the sandbox gateway url.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => 'https://www.smt-sps.com.tn/paiement/'),
+                'sandbox' => array(
+                    'title' => __('Sandbox Mode:', 'yg-smt-woocommerce-payment-gatway'),
+                    'type' => 'checkbox',
+                    'label' => __('Enable Sandbox Mode.', 'yg-smt-woocommerce-payment-gatway'),
+                    'default' => 'no')
+            );
+
         }
 
         /**
          * Receipt Page
          **/
         function receipt_page($order){
-        	echo '<p>'.__('Thank you for your order, please click the button below to pay with SMT Payment Gateway.', 'yg').'</p>';
-        	echo $this->generate_smt_form($order);
-        }
-
-        /**
-         * Process the payment and return the result
-         **/
-        function process_payment($order_id){
-        	$order = new WC_Order($order_id);
-        	return array('result' => 'success', 'redirect' => add_query_arg('order',
-        		$order->id, add_query_arg('key', $order->order_key, get_permalink(get_option('woocommerce_pay_page_id'))))
-        	);
-        }
-
-        /**
-         * Check for valid SMT server callback
-         **/
-        function check_smt_response(){
-        	global $woocommerce;
-        	$reference = (int)$_GET['Reference'];
-        	$action = $_GET['Action'];
-        	$param = $_GET['Param'];
-        	switch($action) {
-        		case "DETAIL":
-        		if ($order = new WC_Order($reference)) {
-        			echo "Reference=".$reference."&Action=".$action."&Reponse=".$order->order_total;
-        			die();
-        		}
-        		break;
-
-        		case "ERREUR":
-        		echo "Reference=".$reference. "&Action=".$action."&Reponse=OK";
-        		break;
-
-        		case "ACCORD":
-        		$order->payment_complete();
-        		$order->add_order_note('SMT payment successful<br/>Transaction Id: '.$param);
-        		$woocommerce->cart->empty_cart();
-
-        		echo "Reference=".$reference. "&Action=".$action."&Reponse=OK";
-        		break;
-
-        		case "REFUS":
-        		echo "Reference=".$reference. "&Action=".$action."&Reponse=OK";
-        		break;
-
-        		case "ANNULATION":
-        		echo "Reference=".$reference. "&Action=".$action."&Reponse=OK";
-        		break;
-        	}
+            echo '<p>'.__('Thank you for your order, please click the button below to pay with SMT Payment Gateway.', 'yg-smt-woocommerce-payment-gatway').'</p>';
+            echo $this->generate_smt_form($order);
         }
 
         /**
          * Generate SMT button link
          **/
         public function generate_smt_form($order_id){
-        	global $woocommerce;
-        	$order = new WC_Order($order_id);
-        	/*
-        	 <input type="hidden" name="affilie" value="{$affiliate}" />
-			  <input type="hidden" name="Devise" value="{$currency}" />
-			  <input type="hidden" name="Reference" value="{$reference}" />
-			  <input type="hidden" name="Montant" value="{$total}" />
-			  <input type="hidden" name="sid" value="{$session_id}" />
-			*/
-		   $smt_args = array(
-              'affilie' => $this->affilie,
-              'devise' => 'TND', // TND = Tunisian Dinar (which is the tunisian currency
-              'reference' => $order_id,
-              'montant' => number_format($order->order_total, 3, '.', ''),
-              'sid' => $order_id); // this is the session ID 
+            $order = new WC_Order($order_id);
+            $smt_args = array(
+                'affilie' => $this->affilie,
+                'devise' => get_woocommerce_currency(),
+                'reference' => $order_id,
+                'montant' => number_format($order->order_total, 3, '.', ''),
+                'sid' => $order_id,
+                'sandbox' => $this->get_option('sandbox')
+            );
 
-    		$smt_args_array = array();
-    		foreach($smt as $key => $value){
-    			$smt_args_array[] = "<input type='hidden' name='$key' value='$value'/>";
-    		}
-    		return '<form action="'.$this->liveurl.'" method="post" id="smt_payment_form">
-    		' . implode('', $smt_args_array) . '
-    		<input type="submit" class="button-alt" id="submit_smt_payment_form" value="'.__('Pay via smt', 'yg').'" /> <a class="button cancel" href="'.$order->get_cancel_order_url().'">'.__('Cancel order &amp; restore cart', 'yg').'</a>
-    		
-    		</form>';
+            $smt_args_array = array();
+            foreach($smt_args as $key => $value){
+                $smt_args_array[] = "<input type='hidden' name='$key' value='$value'/>";
+            }
+            return '<form action="'.$this->liveurl.'" method="post" id="smt_payment_form">
+        ' . implode('', $smt_args_array) . '
+        <input type="submit" class="button-alt" id="submit_smt_payment_form" value="'.__('Pay via SMT', 'yg-smt-woocommerce-payment-gatway').'" /> <a class="button cancel" href="'.$order->get_cancel_order_url().'">'.__('Cancel order &amp; restore cart', 'yg-smt-woocommerce-payment-gatway').'</a>
+        </form>';
+
+
         }
-        // get all pages
-        function get_pages($title = false, $indent = true) {
-        	$wp_pages = get_pages('sort_column=menu_order');
-        	$page_list = array();
-        	if ($title) $page_list[] = $title;
-        	foreach ($wp_pages as $page) {
-        		$prefix = '';
-                // show indented child pages?
-        		if ($indent) {
-        			$has_parent = $page->post_parent;
-        			while($has_parent) {
-        				$prefix .=  ' - ';
-        				$next_page = get_page($has_parent);
-        				$has_parent = $next_page->post_parent;
-        			}
-        		}
-                // add to page list array array
-        		$page_list[$page->ID] = $prefix . $page->post_title;
-        	}
-        	return $page_list;
+
+
+        /**
+         * Admin Panel Options
+         **/
+        public function admin_options(){
+            $ipn_url = site_url('/wc-api/'.get_class( $this ).'/');
+
+            echo '<h3>'.__('SMT Payment Gateway', 'yg-smt-woocommerce-payment-gatway').'</h3>';
+            echo '<p>'.__('', 'yg-smt-woocommerce-payment-gatway').'</p>';
+            echo '<p>'.__( 'Notification URL:', 'yg-smt-woocommerce-payment-gatway' ).' <b><i>'.$ipn_url.'</i></b></p>';
+            echo '<p>'.__( 'Success URL:', 'yg-smt-woocommerce-payment-gatway' ).' <b><i>'.$ipn_url.'?Action=SUCCESS</i></b></p>';
+            echo '<p>'.__( 'ERROR URL:', 'yg-smt-woocommerce-payment-gatway' ).' <b><i>'.$ipn_url.'?Action=ERROR</i></b></p>';
+
+            echo '<table class="form-table">';
+            $this->generate_settings_html();
+            echo '</table>';
+
+        }
+        /**
+         * Process the payment and return the result
+         **/
+        function process_payment($order_id){
+            $order = new WC_Order($order_id);
+            return array('result' => 'success', 'redirect' => add_query_arg('order',
+                $order->id, add_query_arg('key', $order->order_key, get_permalink(get_option('woocommerce_pay_page_id'))))
+            );
+        }
+        /**
+         * Check for valid SMT server callback
+         **/
+        function check_smt_response(){
+            global $woocommerce;
+
+            $reference = (int)$_GET['Reference'];
+            $action = $_GET['Action'];
+            $param = $_GET['Param'];
+            $order = new WC_Order($reference);
+
+            switch($action) {
+                case "DETAIL":
+                    die("Reference=".$reference."&Action=".$action."&Reponse=".$order->order_total);
+                    break;
+
+                case "ERREUR":
+                    die("Reference=".$reference. "&Action=".$action."&Reponse=OK");
+                    break;
+
+                case "ACCORD":
+                    $order->payment_complete();
+                    $order->add_order_note('SMT payment successful<br/>Transaction Id: '.$param);
+                    $woocommerce->cart->empty_cart();
+
+                    die("Reference=".$reference. "&Action=".$action."&Reponse=OK");
+                    break;
+
+                case "REFUS":
+                    die("Reference=".$reference. "&Action=".$action."&Reponse=OK");
+                    break;
+
+                case "ANNULATION":
+                    die("Reference=".$reference. "&Action=".$action."&Reponse=OK");
+                    break;
+
+                case "SUCCESS":
+                    $customer_orders = get_posts( array(
+                        'numberposts' => -1,
+                        'meta_key'    => '_customer_user',
+                        'meta_value'  => get_current_user_id(),
+                        'post_type'   => wc_get_order_types(),
+                        'post_status' => array_keys( wc_get_order_statuses() ),
+                    ) );
+                    $order = new WC_Order( $customer_orders[ 0 ]->ID );
+                    wp_redirect( site_url('/checkout/'.get_option( 'woocommerce_checkout_order_received_endpoint' ).'/'.$order->id.'/?key='.$order->order_key));
+
+                    die;
+                    break;
+
+                case "ERROR":
+                    wc_add_notice( __('Payment error:', 'yg-smt-woocommerce-payment-gatway') . __(' An error occurred during the payment process', 'yg-smt-woocommerce-payment-gatway'), 'error' );
+                    $customer_orders = get_posts( array(
+                        'numberposts' => -1,
+                        'meta_key'    => '_customer_user',
+                        'meta_value'  => get_current_user_id(),
+                        'post_type'   => wc_get_order_types(),
+                        'post_status' => array_keys( wc_get_order_statuses() ),
+                    ) );
+                    $order = new WC_Order( $customer_orders[ 0 ]->ID );
+                    wp_redirect( site_url('/checkout/?order='.$order->id.'&key='.$order->order_key));
+
+                    die;
+                    break;
+            }
         }
     }
-
-    /*
-    * This action hook registers our PHP class as a WooCommerce payment gateway
-    */
-    function woocommerce_add_yg_smt_gateway($methods) {
-    	$methods[] = 'WC_Yg_SMT';
-    	return $methods;
-    }
-
-    add_filter('woocommerce_payment_gateways', 'woocommerce_add_yg_smt_gateway' );
 }
 
 ?>
